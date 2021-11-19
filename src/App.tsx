dfe3958688be30f89c55c8744187363f369de0da
Saimon Moore <saimonmoore@gmail.com>
2021-11-19 23:33:09 +0100

    Initial commit
    Added PanZoom for infinite panning and zooming

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { PanZoom } from 'react-easy-panzoom'
 import logo from './logo.svg';
 import './App.css';
 
-interface AppProps {}
+interface AppProps { }
 
-function App({}: AppProps) {
+function App({ }: AppProps) {
   // Create the count state.
   const [count, setCount] = useState(0);
   // Create the counter (+1 every second).
@@ -16,23 +17,25 @@ function App({}: AppProps) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+        <PanZoom>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <p>
+            Page has been open for <code>{count}</code> seconds.
+          </p>
+          <p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </p>
+        </PanZoom>
       </header>
     </div>
   );
